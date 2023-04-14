148f1949838d0d51edfc07b1fe5b3a8bf0ae0ea0
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2023-04-14 15:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function letterCombinations(digits) {
-  // Define the mapping of digits to letters
+  if (!digits) {
+    return [];
+  }
+
   const map = {
     '2': 'abc',
     '3': 'def',
@@ -11,31 +14,22 @@ function letterCombinations(digits) {
     '9': 'wxyz'
   };
 
-  // Base case: empty input string
-  if (digits.length === 0) {
-    return [];
+  if (digits.length === 1) {
+    return map[digits].split('');
   }
 
-  // Recursive case: split the input into a prefix and a suffix
-  const prefix = digits[0];
+  const first = map[digits[0]];
   const suffix = digits.slice(1);
-
-  // Recursively compute the letter combinations for the suffix
   const suffixCombinations = letterCombinations(suffix);
+  const result = [];
 
-  // Generate the letter combinations for the prefix
-  const prefixLetters = map[prefix];
-  const prefixCombinations = [];
+  for (let i = 0; i < first.length; i++) {
+    const letter = first[i];
 
-  for (let i = 0; i < prefixLetters.length; i++) {
     for (let j = 0; j < suffixCombinations.length; j++) {
-      prefixCombinations.push(prefixLetters[i] + suffixCombinations[j]);
+      result.push(letter + suffixCombinations[j]);
     }
   }
 
-  // Return the letter combinations for the input string
-  return prefixCombinations;
+  return result;
 }
-
-
-module.exports = letterCombinations;
